5147eeddb9b04a5b9acb47f870ef72aedf3ee432
WilferSalas <sony634@hotmail.com>
2024-02-27 19:24:45 -0500

    update multer again

--- src/utils/multerConfig.ts
+++ src/utils/multerConfig.ts
@@ -5,8 +5,7 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadsDir = path.resolve(__dirname, 'uploads')
-    cb(null, uploadsDir)
+    cb(null, path.join(__dirname, 'uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
